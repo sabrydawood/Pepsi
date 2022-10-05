54fcdc4423451952c8b66bb217b7362ad9133870
Sabry Dawood <72863390+virgel1995@users.noreply.github.com>
2022-10-05 17:22:39 +0000

    Insert files

--- src/helpers/BotUtils.js
+++ src/helpers/BotUtils.js
@@ -6,7 +6,7 @@ module.exports = class BotUtils {
    * Check if the bot is up to date
    */
   static async checkForUpdates() {
-    const response = await getJson("https://api.github.com/repos/saiteja-madha/discord-js-bot/releases/latest");
+    const response = await getJson("https://api.github.com/repos/virgel1995/Pepsi/releases/latest");
     if (!response.success) return error("VersionCheck: Failed to check for bot updates");
     if (response.data) {
       if (
@@ -15,7 +15,7 @@ module.exports = class BotUtils {
         success("VersionCheck: Your discord bot is up to date");
       } else {
         warn(`VersionCheck: ${response.data.tag_name} update is available`);
-        warn("download: https://github.com/virgel1995/pepsi/releases/latest");
+        warn("download: https://github.com/virgel1995/Pepsi/releases/latest");
       }
     }
   }
